cfb02f671b431a9a1716e32c431403da9173ff73
Yahya Poernama <yahyapoernama@gmail.com>
2026-07-13 11:49:18 +0700

    feat: add excel maatwebsite

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
             'view-logs' => \App\Http\Middleware\ViewLogs::class,
+            'Excel' => \Maatwebsite\Excel\Facades\Excel::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
